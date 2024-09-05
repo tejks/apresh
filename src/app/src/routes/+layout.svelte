0e677db5502d4fda6b93adc03d2dc581822636ed
Mateusz Zając <matzayonc@gmail.com>
2024-09-05 14:42:09 +0200

    background map

--- src/app/src/routes/+layout.svelte
+++ src/app/src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script>
+	import { GeolocateControl, MapLibre } from 'svelte-maplibre';
 	import '../styles.scss';
 </script>
 
 <main>
-	<slot />
+	<MapLibre
+		style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+		class="maplibre relative w-full sm:aspect-video h-screen z-0"
+		zoom={15}
+		center={[20.98834, 52.23666]}
+	>
+		<GeolocateControl
+			position="top-left"
+			fitBoundsOptions={{ maxZoom: 12 }}
+			showAccuracyCircle={false}
+		/>
+	</MapLibre>
+
+	<!-- <slot /> -->
 </main>
