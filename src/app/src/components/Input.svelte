5679200d9c05eb4c78888afad03a1d327ca13f22
Mateusz Zając <matzayonc@gmail.com>
2024-09-05 18:00:14 +0200

    input component

--- src/app/src/components/Input.svelte
+++ src/app/src/components/Input.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import { on } from 'svelte/events';
 
-	interface ButtonProps {
-		onClick: () => void;
-		children: Snippet;
+	let { onEnter, value = $bindable('') }: { onEnter: (text: string) => void; value: string } =
+		$props();
+
+	function onChange() {
+		onEnter(value);
 	}
-
-	const { children, onClick }: ButtonProps = $props();
 </script>
 
 <div
 	class="flex rounded-full mx-auto bg-gradient-to-tr from-blue-500 via-orange-400 to-rose-400 p-0.5 shadow-lg transition ease-in-out hover:-translate-y-0.5 hover:scale-105 duration-200"
 >
-	<input type="text" onclick={onClick} class="flex-1 bg-white px-7 py-2 rounded-full text-lg" />
+	<input
+		type="text"
+		onchange={() => onEnter(value)}
+		bind:value
+		class="flex-1 bg-white px-7 py-2 rounded-full text-lg"
+	/>
 </div>
