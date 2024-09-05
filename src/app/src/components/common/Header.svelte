52d0c91ae857a69ddf7de4ecdf3c620367f4a594
tejks <sarmatys.programmer@gmail.com>
2024-09-05 15:30:42 +0200

    feat: Add wallet to header

---
+++ src/app/src/components/common/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { wallet } from '$lib/wallet';
+</script>
+
+<header class="sticky top-0 z-50 w-full">
+	<div class="flex items-center px-8 py-4">
+		<div class="ml-auto flex justify-center space-x-5">
+			{#if $wallet.connected && $wallet.identity}
+				<button on:click={() => {}}>
+					Identity: {$wallet.identity.getPrincipal().toText().substring(0, 6)}...
+				</button>
+			{:else}
+				<button on:click={async () => await wallet.connect()}>Connect wallet</button>
+			{/if}
+		</div>
+	</div>
+</header>
--- src/app/src/lib/wallet.ts
+++ src/app/src/lib/wallet.ts
@@ -1,26 +1,26 @@
-import { writable } from "svelte/store";
-import { connect } from "./canisters";
-import type { ActorSubclass, Identity } from "@dfinity/agent";
+import type { ActorSubclass, Identity } from '@dfinity/agent';
+import { writable } from 'svelte/store';
 import type { _SERVICE } from '../../../declarations/contract/contract.did';
+import { connect } from './canisters';
 
-export const wallet = createWallet()
+export const wallet = createWallet();
 
 interface Wallet {
-    connected: true,
-    actor: ActorSubclass<_SERVICE>,
-    identity: Identity
+	connected: true;
+	actor: ActorSubclass<_SERVICE>;
+	identity: Identity;
 }
 
 function createWallet() {
-    const { subscribe, set, update } = writable<{ connected: false } | Wallet>({
-        connected: false,
-    });
+	const { subscribe, set, update } = writable<{ connected: false } | Wallet>({
+		connected: false
+	});
 
-    return {
-        subscribe,
-        connect: async () => {
-            const { actor, identity } = await connect()
-            set({ connected: true, actor, identity })
-        },
-    };
+	return {
+		subscribe,
+		connect: async () => {
+			const { actor, identity } = await connect();
+			set({ connected: true, actor, identity });
+		}
+	};
 }
