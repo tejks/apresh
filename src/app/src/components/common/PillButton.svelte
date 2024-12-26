d5a28267fdee7fa5336635ccfe31e49b1d1361e3
Mateusz Zając <matzayonc@gmail.com>
2024-12-26 13:22:48 +0100

    used button component and fixed markers

--- src/app/src/components/common/PillButton.svelte
+++ src/app/src/components/common/PillButton.svelte
@@ -4,15 +4,24 @@
 		text,
 		className = ''
 	} = $props<{
-		onClick: () => void;
+		onClick?: () => void;
 		text: string;
 		className?: string;
 	}>();
 </script>
 
-<button
-	onclick={onClick}
-	class="bg-gradient-to-r from-blue-500 to-rose-400 rounded-full px-7 py-2 text-white text-base transition ease-in-out hover:-translate-y-0.5 hover:scale-105 duration-200 {className}"
->
-	{text}
-</button>
+{#if onClick}
+	<button
+		onclick={onClick}
+		class="rounded-full bg-gradient-to-r from-blue-500 to-rose-400 px-7 py-2 text-base text-white transition duration-200 ease-in-out hover:-translate-y-0.5 hover:scale-105 {className}"
+	>
+		{text}
+	</button>
+{:else}
+	<button
+		type="submit"
+		class="rounded-full bg-gradient-to-r from-blue-500 to-rose-400 px-7 py-2 text-base text-white transition duration-200 ease-in-out hover:-translate-y-0.5 hover:scale-105 {className}"
+	>
+		{text}
+	</button>
+{/if}
--- src/app/src/components/forms/SettleShipment.svelte
+++ src/app/src/components/forms/SettleShipment.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PillButton from '$components/common/PillButton.svelte';
 	import { getLocalStorage } from '$lib/storage';
 	import QrCodeDisplay from '../QrCodeDisplay.svelte';
 	import ShipmentInfo from '../ShipmentInfo.svelte';
@@ -11,16 +12,11 @@
 	let message = '';
 </script>
 
-<div class="flex justify-between w-full mx-5">
+<div class="mx-5 flex w-full justify-between">
 	<div class="flex flex-col">
 		<ShipmentInfo shipment={selected} />
 
-		<button
-			class="bg-gradient-to-r from-blue-500 to-rose-400 rounded-full px-7 py-2 w-1/2 mx-auto text-white text-base transition ease-in-out hover:-translate-y-0.5 hover:scale-105 duration-200"
-			onclick={() => onSettle(selected!)}
-		>
-			Settle
-		</button>
+		<PillButton text="Settle" onClick={() => onSettle(selected!)} />
 	</div>
 	<div class="flex items-center text-lg">OR</div>
 	<div class="flex items-center">
