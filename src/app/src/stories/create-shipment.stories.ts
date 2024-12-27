1f4860459189e1eaf7c252f2b5debc74f17996b9
Mateusz Zając <matzayonc@gmail.com>
2024-12-27 13:59:56 +0100

    storybook of the create page

--- src/app/src/stories/create-shipment.stories.ts
+++ src/app/src/stories/create-shipment.stories.ts
@@ -1,24 +1,31 @@
-// import type { Meta, StoryObj } from '@storybook/svelte';
+import type { Meta, StoryObj } from '@storybook/svelte';
 // import CreateShipment from '$components/forms/CreateShipment.svelte';
-// import type { SvelteComponent } from 'svelte';
+import CreateShipment from '$routes/shipment/create/+page.svelte';
+import type { SvelteComponent } from 'svelte';
+import type { PageData } from '../routes/shipment/create/$types';
 
-// const meta = {
-// 	title: 'Forms/CreateShipment',
-// 	component: CreateShipment,
-// 	tags: ['autodocs']
-// } satisfies Meta<SvelteComponent>;
+const meta = {
+	title: 'Forms/CreateShipment',
+	component: CreateShipment,
+	tags: ['autodocs']
+} satisfies Meta<SvelteComponent>;
 
-// export default meta;
-// type Story = StoryObj<typeof meta>;
+export default meta;
+type Story = StoryObj<typeof meta>;
 
-// // Basic story showing the create shipment form
-// export const Basic: Story = {
-// 	args: {
-// 		showModal: true,
-// 		onClose: () => {
-// 			console.log('closeHandler');
-// 		}
-// 	}
-// };
+const data: PageData = {
+	created: [],
+	shipments: [],
+	carried: [],
+	registeredCarrier: false,
+	registeredCustomer: false,
+	settleSecret: null,
+	settleId: null
+};
 
-// TODO: migrate to page stories
+// Basic story showing the create shipment form
+export const Basic: Story = {
+	args: {
+		data
+	}
+};
